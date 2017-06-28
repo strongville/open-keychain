70a668a49c58b3d50a6b94e783ddfd47d835e598
Vincent Breitmoser <look@my.amazin.horse>
2017-06-28 14:41:03 +0200

    add context menu to autocrypt identity items

--- OpenKeychain/src/main/java/org/sufficientlysecure/keychain/provider/AutocryptPeerDataAccessObject.java
+++ OpenKeychain/src/main/java/org/sufficientlysecure/keychain/provider/AutocryptPeerDataAccessObject.java
@@ -150,4 +150,8 @@ public class AutocryptPeerDataAccessObject {
         cv.put(ApiAutocryptPeer.STATE, status);
         mQueryInterface.update(ApiAutocryptPeer.buildByPackageNameAndAutocryptId(packageName, autocryptId), cv, null, null);
     }
+
+    public void delete(String autocryptId) {
+        mQueryInterface.delete(ApiAutocryptPeer.buildByPackageNameAndAutocryptId(packageName, autocryptId), null, null);
+    }
 }
